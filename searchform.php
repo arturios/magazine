81ef8a41034fa1c4a47841e014782c3ff1a046ec
Arturo Ríos <arturios@gmail.com>
2013-08-12 19:44:10 +0200

    Revert "frameword para crear revistas ultrafluidas"
    This reverts commit 6756eed8d67371b7ec1bcf8e10f04aa7e66fcf94.

--- searchform.php
+++
@@ -1,4 +0,0 @@
-<form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
-<input type="text" id="s" name="s" value="" />
-<input type="submit" value="Buscar" id="searchsubmit" />
-</form>
