752b6a088c8dae083102db37df61c5dacaa8d384
Arturo Ríos <arturios@gmail.com>
2013-08-12 19:51:37 +0200

    magazine

---
+++ searchform.php
@@ -0,0 +1,4 @@
+<form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
+<input type="text" id="s" name="s" value="" />
+<input type="submit" value="Buscar" id="searchsubmit" />
+</form>
